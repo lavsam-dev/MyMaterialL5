c71fe9901a79de6568774a975aff4dbad6e688d8
lavsam-dev <lavsam@gmail.com>
2021-09-25 11:14:47 +0400

    TransitionManager Explode

--- app/src/main/java/com/learn/lavsam/mymaterial/ui/MainActivity2.kt
+++ app/src/main/java/com/learn/lavsam/mymaterial/ui/MainActivity2.kt
@@ -1,10 +1,9 @@
 package com.learn.lavsam.mymaterial.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
 import android.view.View
-import androidx.transition.Slide
+import androidx.appcompat.app.AppCompatActivity
+import androidx.transition.AutoTransition
 import androidx.transition.TransitionManager
 import com.learn.lavsam.mymaterial.R
 import kotlinx.android.synthetic.main.activity_main2.*
@@ -18,9 +17,11 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
 
         btn.setOnClickListener {
-            TransitionManager.beginDelayedTransition(container, Slide(Gravity.END))
+            TransitionManager.beginDelayedTransition(container, AutoTransition())
             textIsVisible = !textIsVisible
             text.visibility = if (textIsVisible) View.VISIBLE else View.GONE
+            text2.visibility = if (textIsVisible) View.GONE else View.VISIBLE
+            image.visibility = if (textIsVisible) View.VISIBLE else View.GONE
         }
     }
 }
